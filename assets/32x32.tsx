5b435c8d123c88d3b3aad9ed188c59acee135751
rhage-CS <rhage@ucsc.edu>
2026-05-28 14:38:55 -0700

    Added Assets

---
+++ assets/32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="32x32" tilewidth="32" tileheight="32" tilecount="2192" columns="16">
+ <image source="Icons/Free - Raven Fantasy Icons/Full Spritesheet/32x32.png" trans="ff00ff" width="512" height="4384"/>
+</tileset>
